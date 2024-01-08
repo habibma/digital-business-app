cb8697b5e46afce066fd7a97d37f839664354b12
habib motavassel <habib.motavassel@gmail.com>
2024-01-08 17:42:49 +0100

    to modify some bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 import GitHub from '../assets/github.svg'
-import Codepen from '../assets/Codepen.svg'
+import Codepen from '../assets/codepen.svg'
 import Linkedin from '../assets/linkedin icon.svg'
-import Whatsapp from '../assets/Whatsapp.svg'
+import Whatsapp from '../assets/whatsapp.svg'
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <a href='https://github.com/habibma' target='_blank' title='GitHub'><img src={GitHub}/></a>
-            <a href='https://codepen.io/Habib-Mote' target='_blank' title='Codepen'><img src={Codepen} /></a>
-            <a href='https://www.linkedin.com/in/habib-motavassel/' target='_blank' title='LinkedIn'><img src={Linkedin} /></a>
-            <a href='https://wa.me/491622382481' target='blank'><img src={Whatsapp} title='WhatsApp'/></a>
+            <a href='https://github.com/habibma' target='_blank' title='GitHub'><img src={GitHub} alt='GitHub icon'/></a>
+            <a href='https://codepen.io/Habib-Mote' target='_blank' title='Codepen'><img src={Codepen} alt='Codepen icon' /></a>
+            <a href='https://www.linkedin.com/in/habib-motavassel/' target='_blank' title='LinkedIn'><img src={Linkedin} alt='linkedin icon' /></a>
+            <a href='https://wa.me/491622382481' target='blank'><img src={Whatsapp} title='WhatsApp' alt='Whatsapp icon'/></a>
         </div>
     )
 }
